aebcf3b18d970d400a3fd8efe5c77873eb052559
Alexei Bushuev <alexeib.2018@gmail.com>
2019-03-07 06:37:04 +0300

    added fields mapping

--- cgi/roster.pl
+++ cgi/roster.pl
@@ -66,3 +66,56 @@ close FH;
 #print "pretty:\n".JSON->new->pretty->encode(\@arr);
 
 0;
+
+<<EOF
+[{
+	"Id":1,												# position
+	"Date":"2019-03-02T00:00:00-05:00",					# schedule date
+	"StartTime":1551535200,								# schedule start time
+	"EndTime":1551564000,								# schedule end time
+	"Mealbreak":"2019-03-04T00:30:00-05:00",			# schedule meal break (Total)
+	"Slots":[{
+		"blnEmptySlot":false,
+		"strType":"B",
+		"intStart":0,
+		"intEnd":1800,
+		"intUnixStart":1551535200,
+		"intUnixEnd":1551537000,
+		"mixedActivity":{
+			"intState":3,
+			"blnCanStartEarly":1,
+			"blnCanEndEarly":1,
+			"blnIsMandatory":1,
+			"strBreakType":"M"},
+		"strTypeName":"Meal Break",
+		"strState":"Scheduled Duration"}],
+	"TotalTime":7.5,								 	# schedule total time
+	"Cost":0,
+	"OperationalUnit":1,
+	"Employee":1,
+	"Comment":"Test version",						 	# Display name
+	"Warning":"",
+	"WarningOverrideComment":"",
+	"Published":false,
+	"MatchedByTimesheet":1,
+	"Open":false,
+	"ConfirmStatus":0,
+	"ConfirmComment":"",
+	"ConfirmBy":0,
+	"ConfirmTime":0,
+	"SwapStatus":0,
+	"SwapManageBy":null,
+	"ShiftTemplate":1,
+	"ConnectStatus":null,
+	"Creator":1,
+	"Created":"2019-03-02T10:56:44-05:00",
+	"Modified":"2019-03-02T10:58:25-05:00",
+	"OnCost":0,
+	"StartTimeLocalized":"2019-03-02T09:00:00-05:00",	# timesheet start time
+	"EndTimeLocalized":"2019-03-02T17:00:00-05:00",		# timesheet end time
+	"ExternalId":null,
+	"ConnectCreator":null
+}]
+
+Schedule (roster): Columns (in order): Display name, schedule date, schedule start time, schedule end time, schedule meal break (Total), schedule total time, timesheet start time, timesheet end time, position
+EOF
--- cgi/timesheet.pl
+++ cgi/timesheet.pl
@@ -64,3 +64,86 @@ close FH;
 #print "pretty:\n".JSON->new->pretty->encode(\@arr);
 
 0;
+
+<<EOF
+[{
+	"Id":1,
+	"Employee":1,										# employee payroll export code
+	"EmployeeHistory":2566,
+	"EmployeeAgreement":3,
+	"Date":"2019-03-02T00:00:00-05:00",					# timesheet date
+	"StartTime":1551502800,								# timesheet start time
+	"EndTime":1551520800,								# timesheet end time
+	"Mealbreak":"2019-03-02T01:00:00-05:00",
+	"MealbreakSlots":"",
+	"Slots":[{
+		"blnEmptySlot":false,
+		"strType":"B",
+		"intStart":3600,
+		"intEnd":5400,
+		"intUnixStart":1551506400,
+		"intUnixEnd":1551508200,
+		"mixedActivity":{
+			"intState":4,
+			"blnCanStartEarly":1,
+			"blnCanEndEarly":1,
+			"blnIsMandatory":1,
+			"strBreakType":"M"},
+		"strTypeName":"Meal Break",
+		"strState":"Finished Duration"},
+		{
+		"blnEmptySlot":false,
+		"strType":"B",
+		"intStart":3600,
+		"intEnd":5400,
+		"intUnixStart":1551506400,
+		"intUnixEnd":1551508200,
+		"mixedActivity":{
+			"intState":4,
+			"blnCanStartEarly":1,
+			"blnCanEndEarly":1,
+			"blnIsMandatory":1,
+			"strBreakType":"M"},
+		"strTypeName":"Meal Break",
+		"strState":"Finished Duration"}],
+	"TotalTime":4,										# timesheet total time
+	"TotalTimeInv":4,
+	"Cost":0,
+	"Roster":1,
+	"EmployeeComment":null,								# Display name
+	"SupervisorComment":null,
+	"Supervisor":null,
+	"Disputed":false,
+	"TimeApproved":true,
+	"TimeApprover":1,
+	"Discarded":null,
+	"ValidationFlag":0,
+	"OperationalUnit":1,
+	"IsInProgress":null,
+	"IsLeave":false,
+	"LeaveId":null,
+	"LeaveRule":null,
+	"Invoiced":false,
+	"InvoiceComment":null,
+	"PayRuleApproved":true,
+	"Exported":null,
+	"StagingId":null,
+	"PayStaged":false,
+	"PaycycleId":5798,
+	"File":null,
+	"CustomFieldData":null,
+	"RealTime":false,
+	"AutoProcessed":false,
+	"AutoRounded":false,
+	"AutoTimeApproved":false,
+	"AutoPayRuleApproved":true,
+	"Creator":1,
+	"Created":"2019-03-02T10:58:25-05:00",
+	"Modified":"2019-03-02T10:58:25-05:00",
+	"OnCost":0,
+	"StartTimeLocalized":"2019-03-02T00:00:00-05:00",	schedule start time
+	"EndTimeLocalized":"2019-03-02T05:00:00-05:00"		schedule end time
+}]
+
+Timesheet: Columns (in order): Display name, timesheet date, timesheet start time, timesheet end time, timesheet total time, schedule start time, schedule end time, employee payroll export code
+EOF
