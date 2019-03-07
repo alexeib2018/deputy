84dc51eccdde30dbbb4ab7b1d279d0f4c05ef933
Alexei Bushuev <alexeib.2018@gmail.com>
2019-03-07 06:33:58 +0300

    developed timesheet.pl output to timesheet.csv

--- cgi/timesheet.pl
+++ cgi/timesheet.pl
@@ -2,6 +2,8 @@
 use strict;
 use warnings;
 use Net::Curl::Easy;
+#use JSON;
+use JSON::Parse 'parse_json';
 
 require "settings.pl";
 our $endpoint;
@@ -32,4 +34,33 @@ $curl->setopt(Net::Curl::Easy::CURLOPT_HTTPHEADER(), [ 'Content-type: applicatio
 													   'Authorization : OAuth ' . $token,
 													   'dp-meta-option : none' ]);
 
+my $response_body;
+$curl->setopt(Net::Curl::Easy::CURLOPT_WRITEDATA(), \$response_body);
+
 my $retcode = $curl->perform;
+
+my $filename = "timesheet.csv";
+open(FH, '>', $filename) or die $!;
+
+my @json_arr = parse_json($response_body);
+
+my @arr = @{$json_arr[0]};
+my $arr_size = scalar @arr;
+for(my $i=0; $i<$arr_size; $i++) {
+	my %rec = %{$arr[$i]};
+	my $display_name = $rec{"EmployeeComment"} ? $rec{"EmployeeComment"} : "";
+	my $timesheet_date = $rec{"Date"};
+	my $timesheet_start_time = $rec{"StartTime"};
+	my $timesheet_end_time = $rec{"EndTime"};
+	my $timesheet_total_time = $rec{"TotalTime"};
+	my $schedule_start_time = $rec{"StartTimeLocalized"};
+	my $schedule_end_time = $rec{"EndTimeLocalized"};
+	my $employee_payroll_export_code = $rec{"Employee"};
+
+	print FH "$display_name,$timesheet_date,$timesheet_start_time,$timesheet_end_time,$timesheet_total_time,$schedule_start_time,$schedule_end_time,$employee_payroll_export_code\n";
+}
+
+close FH;
+#print "pretty:\n".JSON->new->pretty->encode(\@arr);
+
+0;
